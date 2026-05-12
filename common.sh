f18d2ea9576367eefd07f0cc01c8dc5f629aba37
CodingManoj <mvp2612@hotmail.com>
2026-05-11 21:41:06 -0400

    Roboshop Configuration Management using Bash

--- common.sh
+++ common.sh
@@ -2,15 +2,17 @@
 
 echo "Configuration Management for $COMPONENT in progress"
 
+ID=$(id -u)
+APPUSER="roboshop"
+LOG="/tmp/${COMPONENT}.log"
+
 if [ $ID -ne 0 ]; then 
     echo -e "\e[35m Script has to executed as a root user or with sudo \e[0m"
     echo -e "Example Usage: \n\t \e[33m sudo bash $0  OR # bash $0 \e[0m"
     exit 1
 fi
 
-ID=$(id -u)
-APPUSER="roboshop"
-LOG="/tmp/${COMPONENT}.log"
+
 
 stat() {
     if [ $1 -eq 0 ]; then 
