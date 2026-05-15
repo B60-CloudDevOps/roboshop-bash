a324240268e515c7f12afae5e6a8e4b75ebc5a3e
CodingManoj <mvp2612@hotmail.com>
2026-05-14 21:56:11 -0400

    Roboshop Configuration Management using Bash

--- common.sh
+++ common.sh
@@ -6,7 +6,7 @@ ID=$(id -u)
 APPUSER="roboshop"
 LOG="/tmp/${COMPONENT}.log"
 
-if [[ "$ID" -ne 0 && -z "$1" ]]; then
+if [[ "$ID" -ne 0 & -z "$1" ]]; then
     echo -e "\e[35m Script has to executed as a root user or with sudo \e[0m"
     echo -e "Example Usage: \n\t \e[33m sudo bash $0 env OR # bash $0 env \e[0m"
     echo "Env Name is $1"
