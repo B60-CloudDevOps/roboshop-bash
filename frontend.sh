33dd6284551e5ad2a58cc7f9ef8733d651e9e90f
CodingManoj <mvp2612@hotmail.com>
2026-05-14 21:34:04 -0400

    Roboshop Configuration Management using Bash

--- frontend.sh
+++ frontend.sh
@@ -13,7 +13,13 @@ if [ $ID -ne 0 ]; then
     echo -e "\e[35m Script has to executed as a root user or with sudo \e[0m"
     echo -e "Example Usage: \n\t \e[33m sudo bash $0  OR # bash $0 \e[0m"
     exit 1
-fi
+fi 
+
+if [ -z $2 ]; then 
+    echo -e "\e[35m Script has to executed as a root user or with sudo  + environment \e[0m"
+    echo -e "Example Usage: \n\t \e[33m sudo bash $0 environment \e[0m"
+    exit 2
+fi 
 
 stat() {
     if [ $1 -eq 0 ]; then 
