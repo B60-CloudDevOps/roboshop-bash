9cfd89778f90d9b5f6b97f2c8b54872df26938e6
CodingManoj <mvp2612@hotmail.com>
2026-05-14 21:49:26 -0400

    Roboshop Configuration Management using Bash

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@
 
 # I want to make sure that the scirpt has to validate whether the user running the script is root user or not, if not root user, script has to be exited
 COMPONENT="catalogue"
+ENVIRONMENT="$1"
 source ./common.sh
 nodejs
 
